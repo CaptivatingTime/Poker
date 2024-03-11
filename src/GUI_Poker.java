fab6218d393de442b04aad09897fe8a4667b2c46
CaptivatingTime <111361451+CaptivatingTime@users.noreply.github.com>
2024-03-11 18:33:46 +0200

    update

---
+++ src/GUI_Poker.java
@@ -0,0 +1,35 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class GUI_Poker {
+
+    GUI_Poker(){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+        
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
+
+        panel1.setBackground(Color.red);
+        panel2.setBackground(Color.green);
+        panel3.setBackground(Color.yellow);
+        panel4.setBackground(Color.magenta);
+        panel5.setBackground(Color.blue);
+
+        panel1.setPreferredSize(new Dimension(100,100));
+		panel2.setPreferredSize(new Dimension(150,100));
+		panel3.setPreferredSize(new Dimension(150,100));
+		panel4.setPreferredSize(new Dimension(100,100));
+		panel5.setPreferredSize(new Dimension(100,100));
+
+        frame.add(panel1,BorderLayout.NORTH);
+    }
+}
